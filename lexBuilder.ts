31b21403094472376ae40eac97a1ce9b55bcc379
Myles Rankin <me@mylesrankin.uk>
2019-07-26 12:40:16 +0100

    Removed console.logs

--- lexBuilder.ts
+++ lexBuilder.ts
@@ -241,14 +241,14 @@ export class LexBuilder {
       version: version,
       enumerationValues: alexaType.values.map((v) => {
         if (alexaType.strategy == "TOP_RESOLUTION") {
-          console.log("Top Resolution");
+          // console.log("Top Resolution");
           return {
             value: v.name.value,
             synonyms: v.name.synonyms
           };
         } else {
-          console.log("Original Value");
-          console.log("->" + v);
+          // console.log("Original Value");
+          // console.log("->" + v);
           return {
             value: v
           };
